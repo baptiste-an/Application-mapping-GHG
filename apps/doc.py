0dbff701387480d2c1cf93754741e69a8fff7ab3
baptiste-an <baptiste.andrieu@protonmail.com>
2023-01-08 16:18:06 +0100

    links in doc page

--- apps/doc.py
+++ apps/doc.py
@@ -1,10 +1,42 @@
-from dash import html
+from dash import html, dcc
 import dash_bootstrap_components as dbc
 import pathlib
 from app import app
 
 
 DATA_PATH = pathlib.Path(__file__).parent.joinpath("data").resolve()
+
+text = (
+    dcc.Textarea(
+        id="textarea-state-example",
+        value="",
+        style={"width": "100%", "height": 200},
+    ),
+)
+text = dcc.Markdown(
+    """
+    The goal of this project is to build a visual representation of all possible scopes for calculating greenhouse gas footprint. We use EXIOBASE3 - a global multi-regional input-output database - to calculate emissions for 49 regions of the world between 1995 and 2019. 
+    
+    A full explanation of the diagrams is available in the **paper**: not yet posted on preprint server
+
+    The **code** used to calculate the footprints and build the application is available on github: https://github.com/baptiste-an/Mapping-global-ghg-emissions
+
+    The **sankey diagrams in svg format** can be downloaded on github: https://github.com/baptiste-an/Mapping-global-ghg-emissions/tree/main/Results/Sankey_figs
+
+
+"""
+)
+
+# *This text will be italic*
+
+# _This will also be italic_
+
+
+# **This text will be bold**
+
+# __This will also be bold__
+
+# _You **can** combine them_
 
 
 layout = dbc.Container(
@@ -24,7 +56,7 @@ layout = dbc.Container(
                 dbc.Col(
                     [
                         html.A(
-                            html.Img(src=app.get_asset_url("isterre.png"), style={"height": 100, "justify": "center"}),
+                            html.Img(src=app.get_asset_url("isterre2.jpg"), style={"height": 100, "justify": "center"}),
                             href="https://www.isterre.fr/?lang=en",
                             target="_blank",
                         )
@@ -44,7 +76,7 @@ layout = dbc.Container(
                 dbc.Col(
                     [
                         html.A(
-                            html.Img(src=app.get_asset_url("uga.png"), style={"height": 100, "justify": "center"}),
+                            html.Img(src=app.get_asset_url("uga2.jpg"), style={"height": 100, "justify": "center"}),
                             href="https://www.univ-grenoble-alpes.fr/english/",
                             target="_blank",
                         )
@@ -53,6 +85,7 @@ layout = dbc.Container(
                 ),
             ],
         ),
+        html.Div(text, className="border")
         # dbc.Row([dbc.Col([thanks], width=12)])
         # html.Div(thanks, id="page-content"),
     ],

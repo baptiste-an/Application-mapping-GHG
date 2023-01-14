fc129c0d8f327ee72a3445acea679cf72dd4ed88
baptiste-an <baptiste.andrieu@protonmail.com>
2023-01-14 11:32:32 +0100

    funding

--- apps/doc.py
+++ apps/doc.py
@@ -6,38 +6,39 @@ from app import app
 
 DATA_PATH = pathlib.Path(__file__).parent.joinpath("data").resolve()
 
-text = (
-    dcc.Textarea(
-        id="textarea-state-example",
-        value="",
-        style={"width": "100%", "height": 200},
-    ),
-)
+
 text = dcc.Markdown(
     """
+    ##### Documentation
+
     The goal of this project is to build a visual representation of all possible scopes for calculating greenhouse gas footprint. We use EXIOBASE3 - a global multi-regional input-output database - to calculate emissions for 49 regions of the world between 1995 and 2019. 
     
     A full explanation of the diagrams is available in the **paper**: not yet posted on preprint server
 
+    ##### Code and figures downloads
+    
     The **code** used to calculate the **footprints** is available on github: https://github.com/baptiste-an/Mapping-global-ghg-emissions
 
     The **code** to build the **application** is available on github: https://github.com/baptiste-an/Application-mapping-GHG
 
     The **sankey diagrams in svg format** can be downloaded on github: https://github.com/baptiste-an/Mapping-global-ghg-emissions/tree/main/Results/Sankey_figs
 
+    ##### Funding
 
+    Most of this work has been funded by [*The Shift Project*](https://theshiftproject.org/en), a French think tank advocating the shift to a post carbon economy. As a non-profit organisation committed to serving the general interest through scientific objectivity, *The Shift Project* is dedicated to informing and influencing the debate on energy transition in Europe.
+
+    Other funders include [ISTerre]("https://www.isterre.fr/?lang=en"), [STEEP]("https://steep.inria.fr/en/"), [GAEL]("https://gael.univ-grenoble-alpes.fr/?language=en"), [University Grenoble Alpes]("https://www.univ-grenoble-alpes.fr/english/"), [CNRS](https://www.cnrs.fr/en), and the [French National Research Agency](https://anr.fr/en/).
+
+    ##### Citation
+
+    Andrieu, B., Le Boulzec, H., Delannoy, L., Verzier, F., Winter, G., Vidal, O., Mapping global greenhouse gases emissions: an interactive, open-access, web application. Available at:
 """
 )
 
 # *This text will be italic*
-
 # _This will also be italic_
-
-
 # **This text will be bold**
-
 # __This will also be bold__
-
 # _You **can** combine them_
 
 
@@ -98,7 +99,7 @@ layout = dbc.Container(
             ],
             justify="center",
         ),
-        html.Div(text, className="border")
+        html.Div(text, className="border", style={"fontSize": 13})
         # dbc.Row([dbc.Col([thanks], width=12)])
         # html.Div(thanks, id="page-content"),
     ],
